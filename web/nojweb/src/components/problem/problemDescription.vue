6e6f15f068811504a5e11166d3bd5e75a1c7db6a
paradise-lu <1833631539@qq.com>
2024-04-14 14:17:29 +0800

    description

--- web/nojweb/src/components/problem/problemDescription.vue
+++ web/nojweb/src/components/problem/problemDescription.vue
@@ -4,7 +4,7 @@
     </div>
 
     <div class="problem-description">
-      <h1>{{ problem.title }}</h1>
+      <h1 style="text-align: center;">{{ problem.title }}</h1>
       <h2>题目内容</h2>
       <p>{{ problem.content }}</p>
   
@@ -16,10 +16,9 @@
   
       <h2>样例一</h2>
       <h3>Input</h3>
-      <pre>{{ problem.example.input }}</pre>
+      <pre class="code-box">{{ problem.example.input }}</pre>
       <h3>Output</h3>
-      <pre>{{ problem.example.output }}</pre>
-  
+      <pre class="code-box">{{ problem.example.output }}</pre>  
       <h2>限制与约定</h2>
       <p>时间限制：{{ problem.timeLimit }} ms</p>
       <p>空间限制：{{ problem.spaceLimit }} MB</p>
@@ -58,7 +57,6 @@
     methods: {
       fetchProblemData() {
         // Simulated problem data
-        
         axios.get('http://127.0.0.1:8000/problem/' + this.pid).then(res => {
         this.problem = {
           title : res.data.title,
@@ -89,6 +87,10 @@
   text-align: left;
   max-width: 800px;
   margin: 0 auto;
+}
+
+.code-box {
+  background-color: #E8F0FE;
 }
 
   </style>
